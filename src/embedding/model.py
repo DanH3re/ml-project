b4b246962ff7762ac29f5aa66005a13c0a5d0876
Anton <73530867+v10l4c3um@users.noreply.github.com>
2026-03-13 11:14:35 +0000

    Removed unncessary structures

--- src/embedding/model.py
+++
@@ -1,7 +0,0 @@
-from tensorflow.keras.models import Model
-from embedding.layers import build_embedding_layers
-
-
-def build_model(vocab_size: int, d: int, max_seq_length: int) -> Model:
-    inputs, x = build_embedding_layers(vocab_size, d, max_seq_length)
-    return Model(inputs, x)
